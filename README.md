8fd339880b79ef24ae521ff42eca3bf109fa41fa
Espen Røvik Larsen <espenrl@@users.noreply.github.com>
2019-08-02 16:44:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+﻿# Reference Gate
+### - for MSBuild and Common Project System (CPS) based projects
+
+> A reference not approved should end in a build error.
+
+Ever discovered a reference that really shouldn't be in the project. It is there because some code needs a single method call in another assembly - and it really isn't neccesary. Tools like ReSharper makes it far to easy to add an external reference by accident. Reference Gate will make the build fail in cases like this.
+
+> You as a developer will need to maintain a whitelist.
+
+[NuGet package](https://www.nuget.org/packages/refGate)
+
+## Examples
+
+##### Assembly references
+
+Guarding the `Reference` variable.
+
+``` xml
+<AssemblyReferenceWhitelist Include="System.Linq" />
+<AssemblyReferenceBlacklist Include="System.Configuration" />
+```
+
+##### Package references
+
+Guarding the `PackageReference` variable.
+
+``` xml
+<PackageReferenceWhitelist Include="NUnit" />
+<PackageReferenceBlacklist Include="Newtonsoft.Json" />
+```
+
+##### Project references
+
+Guarding the `ProjectReference` variable.
+
+``` xml
+<ProjectReferenceWhitelist Include="..\Newtonsoft.Json\Newtonsoft.Json.csproj" />
+<ProjectReferenceBlacklist Include="..\Newtonsoft.Json.Tests\Newtonsoft.Json.Tests.csproj" />
+```
+
+## Limitations
+* Both blacklisting and whitelisting at the same time is not supported. One method has to be chosen. This applies per project, per type\*.
+* The comparison of references is based on case sensitive string compare. This may pose limits for file paths in assembly references and project references.
+
+\* Type being either assembly, package or project reference.
---
+++ ReferenceGateTask.fs
@@ -0,0 +1,42 @@
+﻿// https://natemcmaster.com/blog/2017/07/05/msbuild-task-in-nuget/
+// http://blog.differentpla.net/blog/2013/02/01/msbuild-tasks-input-parameters-and-itemgroups
+
+namespace ReferenceGate
+
+open Microsoft.Build.Framework
+
+type private Reference = { Name: string }
+
+type ReferenceGateTask() =
+    inherit Microsoft.Build.Utilities.Task()
+
+    [<Required>] member val Type : string = "" with get, set
+    [<Required>] member val ActualReferences   : ITaskItem[] = Array.empty<ITaskItem> with get, set
+    [<Required>] member val ReferenceWhitelist : ITaskItem[] = Array.empty<ITaskItem> with get, set
+    [<Required>] member val ReferenceBlacklist : ITaskItem[] = Array.empty<ITaskItem> with get, set
+
+    override __.Execute() =
+        
+        let map (refs:ITaskItem[]) =
+            refs
+            |> Array.map (fun r -> { Name = r.ItemSpec })
+            |> Set.ofArray
+
+        let a = __.ActualReferences |> map
+        let w = __.ReferenceWhitelist |> map
+        let b = __.ReferenceBlacklist |> map
+
+        match (w.Count, b.Count) with
+        | (0, 0) ->
+            true
+        | (wlen, 0) when wlen > 0 ->
+            let diff = w |> Set.difference a
+            diff |> Set.iter (fun ref -> __.Log.LogError(sprintf "%s '%s' is not on whitelist." __.Type ref.Name))
+            diff |> Set.isEmpty
+        | (0, blen) when blen > 0 ->
+            let diff = a |> Set.intersect b
+            diff |> Set.iter (fun ref -> __.Log.LogError(sprintf "%s '%s' is blacklisted." __.Type ref.Name))
+            diff |> Set.isEmpty
+        | (_, _) ->
+            __.Log.LogError("Can not whitelist and blacklist at the same time. Choose either method.")
+            false
